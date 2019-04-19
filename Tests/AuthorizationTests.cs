9f5086b7b2a888bfc45e1877d55e39259babd6ad
Josh Wingstrom <josh@eastfive.com>
2019-04-19 12:35:06 -0500

    invite resource added

--- Tests/AuthorizationTests.cs
+++ Tests/AuthorizationTests.cs
@@ -44,144 +44,160 @@ namespace EastFive.Azure.Tests.Authorization
             this.GenerateKeys();
         }
 
+
         [TestMethod]
-        public async Task AuthenticationRequestActsAppropriately()
+        public async Task CanLoginInviteAuthenticationRequest()
         {
-            Assert.IsTrue(await await SessionUtilities.StartAsync(
-                async (testSession) =>
-            {
-                Assert.IsTrue(await await testSession.AuthenticationRequestLinksGetAsync(
-                    async (responseGetAccountLinks, fetchAuthRequestLinks) =>
+            // var sessionFactory = new RestApplicationFactory();
+            var sessionFactory = await TestApplicationFactory.InitAsync();
+            var superAdmin = await sessionFactory.SessionSuperAdminAsync();
+
+            (superAdmin as Api.Azure.AzureApplication)
+                .AddOrUpdateInstantiation(typeof(Auth.CredentialProviders.AdminLogin),
+                    async (app) =>
                     {
-                        AssertApi.Success(responseGetAccountLinks);
-                        var authRequestLinks = fetchAuthRequestLinks();
-                        Assert.IsTrue(authRequestLinks.Any());
-                        var authRequestLink = authRequestLinks.First();
+                        await 1.AsTask();
+                        return new Auth.CredentialProviders.AdminLogin();
+                    });
+            (superAdmin as Api.Azure.AzureApplication)
+                .AddOrUpdateInstantiation(typeof(ProvideLoginMock),
+                    async (app) =>
+                    {
+                        await 1.AsTask();
+                        return new ProvideLoginMock();
+                    });
 
-                        var superAdminSession = testSession.GetSuperAdmin();
+            var authenticationAdmin = await superAdmin.GetAsync<Method, Method>(
+                onContents:
+                    authentications =>
+                    {
+                        var matchingAuthentications = authentications
+                            .Where(auth => auth.name == Auth.CredentialProviders.AdminLogin.IntegrationName);
+                        Assert.IsTrue(matchingAuthentications.Any());
+                        return matchingAuthentications.First();
+                    });
 
-                        var redirectAddressDesired = new Uri($"http://testing{Guid.NewGuid().ToString("N")}.example.com/App");
-                        var redirectAddressDesiredPostLogout = new Uri($"http://testing{Guid.NewGuid().ToString("N")}.example.com/Login");
-                        var userSession = await await testSession.SessionPostAsync(authRequestLink.Id,
-                                Enum.GetName(typeof(CredentialValidationMethodTypes), authRequestLink.CredentialValidationMethodType), Security.SessionServer.AuthenticationActions.signin, 
-                                redirectAddressDesired, redirectAddressDesiredPostLogout,
-                            async (responsePosted, postedResource, fetchBody) =>
-                            {
-                                AssertApi.Created(responsePosted);
-                                var authenticationRequestPosted = fetchBody();
-                                Assert.IsFalse(authenticationRequestPosted.AuthorizationId.HasValue);
-                                Assert.IsFalse(authenticationRequestPosted.Token.IsNullOrWhiteSpace());
-                                Assert.AreEqual(Enum.GetName(typeof(CredentialValidationMethodTypes), authRequestLink.CredentialValidationMethodType), authenticationRequestPosted.Method);
-                                Assert.AreEqual(redirectAddressDesired, authenticationRequestPosted.LocationAuthenticationReturn);
-                                Assert.AreEqual(redirectAddressDesiredPostLogout, authenticationRequestPosted.LocationLogoutReturn);
+            var mockAuthenticationMock = await superAdmin.GetAsync<Method, Method>(
+                onContents:
+                    authentications =>
+                    {
+                        var matchingAuthentications = authentications
+                            .Where(auth => auth.name == ProvideLoginMock.IntegrationName);
+                        Assert.IsTrue(matchingAuthentications.Any());
+                        return matchingAuthentications.First();
+                    });
 
-                                // Fetching without a tokened session succeeds... (per UI dev's whining ;-))
-                                AssertApi.Success(await testSession.AuthenticationRequestGetAsync(postedResource,
-                                    (response, fetch) => response));
+            var authReturnUrl = new Uri("http://example.com/authtest");
+            var authorizationIdSecure = Security.SecureGuid.Generate();
+            var authorizationInvite = new Auth.Authorization
+            {
+                authorizationRef = authorizationIdSecure.AsRef<Auth.Authorization>(),
+                Method = mockAuthenticationMock.authenticationId,
+                LocationAuthenticationReturn = authReturnUrl,
+            };
+            var authroizationWithUrls = await superAdmin.PostAsync(authorizationInvite,
+                onCreatedBody:
+                    (authorizationResponse, contentType) =>
+                    {
+                        Assert.AreEqual(authorizationInvite.Method.id, authorizationResponse.Method.id);
+                        Assert.AreEqual(authReturnUrl, authorizationResponse.LocationAuthenticationReturn);
+                        return authorizationResponse;
+                    });
 
-                                ((TestSession)testSession).LoadToken(authenticationRequestPosted.Token);
-                                return await await testSession.AuthenticationRequestGetAsync(postedResource,
-                                    async (responseAuthRequestGet, fetch) =>
+
+            var externalSystemUserId = Guid.NewGuid().ToString();
+            var internalSystemUserId = Guid.NewGuid();
+            // var mockParameters = ProvideLoginMock.GetParameters(externalSystemUserId);
+            Assert.IsTrue(await superAdmin.PostAsync(
+                new Auth.AccountMapping
+                {
+                    accountMappingId = Guid.NewGuid(),
+                    accountId = internalSystemUserId,
+                    authorization = authorizationInvite.authorizationRef,
+                },
+                onCreated: () => true));
+
+            var comms = sessionFactory.GetUnauthorizedSession();
+            var session = new Session
+            {
+                sessionId = Guid.NewGuid().AsRef<Session>(),
+            };
+            var token = await comms.PostAsync(session,
+                onCreatedBody: (sessionWithToken, contentType) => sessionWithToken.token);
+
+            (comms as Api.Azure.AzureApplication)
+                .AddOrUpdateInstantiation(typeof(ProvideLoginMock),
+                    async (app) =>
+                    {
+                        await 1.AsTask();
+                        return new ProvideLoginMock();
+                    });
+
+            // TODO: comms.LoadToken(session.token);
+            var authentication = await comms.GetAsync<Method, Method>(
+                onContents:
+                    authentications =>
+                    {
+                        var matchingAuthentications = authentications
+                            .Where(auth => auth.name == ProvideLoginMock.IntegrationName);
+                        Assert.IsTrue(matchingAuthentications.Any());
+                        return matchingAuthentications.First();
+                    });
+
+            var responseResource = ProvideLoginMock.GetResponse(externalSystemUserId, authorizationInvite.authorizationRef.id);
+            var authorizationToAthenticateSession = await await comms.GetAsync(responseResource,
+                onRedirect:
+                    async (urlRedirect, reason) =>
+                    {
+                        var authIdStr = urlRedirect.GetQueryParam(EastFive.Api.Azure.AzureApplication.QueryRequestIdentfier);
+                        var authId = Guid.Parse(authIdStr);
+                        var authIdRef = authId.AsRef<Auth.Authorization>();
+
+                        // TODO: New comms here?
+                        return await await comms.GetAsync(
+                            (Auth.Authorization authorizationGet) => authorizationGet.authorizationRef.AssignQueryValue(authIdRef),
+                            onContent:
+                                async (authenticatedAuthorization) =>
+                                {
+                                    var sessionVirgin = new Session
                                     {
-                                        AssertApi.Success(responseAuthRequestGet);
-                                        var value = fetch();
-                                        Assert.IsFalse(value.AuthorizationId.HasValue);
-                                        Assert.IsTrue(value.Token.IsNullOrWhiteSpace());
-                                        Assert.AreEqual(Enum.GetName(typeof(CredentialValidationMethodTypes), authRequestLink.CredentialValidationMethodType), value.Method);
-                                        Assert.IsTrue(value.RefreshToken.IsNullOrWhiteSpace());
-                                        Assert.AreEqual(redirectAddressDesired, value.LocationAuthenticationReturn);
-                                        Assert.AreEqual(redirectAddressDesiredPostLogout, value.LocationLogoutReturn);
+                                        sessionId = Guid.NewGuid().AsRef<Session>(),
+                                        authorization = new RefOptional<Auth.Authorization>(authIdRef),
+                                    };
+                                    var tokenNew = await comms.PostAsync(sessionVirgin,
+                                        onCreatedBody: (sessionWithToken, contentType) =>
+                                        {
+                                            Assert.AreEqual(internalSystemUserId, sessionWithToken.account.Value);
+                                            return sessionWithToken.HeaderName.PairWithValue(sessionWithToken.token);
+                                        });
+                                    comms.Headers.Add(tokenNew.Key, tokenNew.Value);
 
-                                        var userIdProvider = Guid.NewGuid().ToString("N");
-                                        var token = ProvideLoginMock.GetToken(userIdProvider);
-                                        Enum.TryParse(authRequestLink.Method, out CredentialValidationMethodTypes val);
-                                        var responseWithoutAccountLink = await testSession.GetAsync<ResponseController>(
-                                            new ResponseResult
-                                            {
-                                                method = val,
-                                            },
-                                            (request) =>
-                                            {
-                                                request.RequestUri = value.LocationAuthentication.ParseQuery().Aggregate(
-                                                    request.RequestUri.AddQuery(ProvideLoginMock.extraParamToken, token),
-                                                    (url, queryValue) => url.AddQuery(queryValue.Key, queryValue.Value));
-                                            });
-                                        AssertApi.Conflict(responseWithoutAccountLink);
-
-                                        var authentication = Guid.NewGuid();
-                                        AssertApi.Created(await superAdminSession.CredentialPostAsync(val, userIdProvider, authentication,
-                                            (response, resource) => response));
-
-                                        var responsePostAccountLink = await testSession.GetAsync<ResponseController>(
-                                            new ResponseResult
-                                            {
-                                                method = val,
-                                            },
-                                            (request) =>
-                                            {
-                                                request.RequestUri = value.LocationAuthentication.ParseQuery().Aggregate(
-                                                    request.RequestUri.AddQuery(ProvideLoginMock.extraParamToken, token),
-                                                    (url, queryValue) => url.AddQuery(queryValue.Key, queryValue.Value));
-                                            });
-                                        AssertApi.Redirect(responsePostAccountLink);
-                                        Assert.AreEqual(
-                                            postedResource.Id.UUID.ToString("N"),
-                                            responsePostAccountLink.Headers.Location.GetQueryParam(EastFive.Api.Azure.AzureApplication.QueryRequestIdentfier));
-
-                                        return await testSession.AuthenticationRequestGetAsync(postedResource,
-                                            (responseAuthRequestPopulatedGet, fetchPopulated) =>
-                                            {
-                                                var authRequestPopulated = fetchPopulated();
-                                                Assert.IsTrue(authRequestPopulated.AuthorizationId.HasValue);
-                                                var userSes = new TestSession(authRequestPopulated.AuthorizationId.Value);
-                                                Assert.IsFalse(authRequestPopulated.Token.IsNullOrWhiteSpace());
-                                                userSes.LoadToken(authRequestPopulated.Token);
-                                                
-                                                return userSes;
-                                            });
-                                    });
-                            });
-
-
-                        var redirectAddressDesiredLink = new Uri($"http://testing{Guid.NewGuid().ToString("N")}.example.com");
-                        Assert.IsTrue(await await userSession.IntegrationPostAsync(Guid.NewGuid(),
-                            Enum.GetName(typeof(CredentialValidationMethodTypes), authRequestLink.CredentialValidationMethodType), userSession.Id, redirectAddressDesiredLink,
-                            async (responsePosted, postedResource, fetchBody) =>
-                            {
-                                AssertApi.Created(responsePosted);
-                                var authenticationRequestPosted = fetchBody();
-                                Assert.AreEqual(userSession.Id, authenticationRequestPosted.AuthorizationId);
-                                Assert.AreEqual(Enum.GetName(typeof(CredentialValidationMethodTypes), authRequestLink.CredentialValidationMethodType), authenticationRequestPosted.Method);
-                                Assert.AreEqual(redirectAddressDesiredLink, authenticationRequestPosted.LocationAuthenticationReturn);
-
-                                var userIdProvider = Guid.NewGuid().ToString("N");
-                                var token = ProvideLoginMock.GetToken(userIdProvider);
-                                Enum.TryParse(authRequestLink.Method, out CredentialValidationMethodTypes val);
-                                var responseWithoutAccountLink = await testSession.GetAsync<ResponseController>(
-                                    new ResponseResult
+                                    var integration = new Auth.Integration
                                     {
-                                        method = val
-                                    },
-                                    (request) =>
-                                    {
-                                        request.RequestUri = authenticationRequestPosted.LocationAuthentication.ParseQuery().Aggregate(
-                                            request.RequestUri.AddQuery(ProvideLoginMock.extraParamToken, token),
-                                            (url, queryValue) => url.AddQuery(queryValue.Key, queryValue.Value));
-                                    });
-                                AssertApi.Redirect(responseWithoutAccountLink);
+                                        integrationRef = Guid.NewGuid().AsRef<Auth.Integration>(),
+                                        accountId = internalSystemUserId,
+                                        Method = mockAuthenticationMock.authenticationId,
+                                        authorization = new RefOptional<Auth.Authorization>(authIdRef),
+                                    };
+                                    Assert.IsTrue(await comms.PostAsync(integration,
+                                        onCreated: () => true));
 
-                                return true;
-                            }));
-                        
-                        return true;
-                    }));
-                return true;
-            }));
+                                    session.authorization = new RefOptional<Auth.Authorization>(authenticatedAuthorization.authorizationRef);
+                                    return await comms.PatchAsync(session,
+                                        onUpdatedBody:
+                                            (updated) =>
+                                            {
+                                                return updated;
+                                            });
+                                });
+                    });
+
+            Assert.AreEqual(internalSystemUserId, authorizationToAthenticateSession.account.Value);
+
         }
 
-
         [TestMethod]
-        public async Task AuthenticationRequestActsAppropriately2()
+        public async Task CanLoginWithAuthenticationRequest()
         {
 
             // var sessionFactory = new RestApplicationFactory();
